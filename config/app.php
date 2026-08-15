3e0cfb653e6fbaed867416b58011c86649b7d98c
Johannes de Haan <johannes@johannesdehaan.com>
2026-08-15 18:40:28 +0000

    Use Norwegian copy so registration is clearly for the ledger.

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => env('APP_TIMEZONE', 'Europe/Oslo'),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => env('APP_LOCALE', 'nb'),
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
